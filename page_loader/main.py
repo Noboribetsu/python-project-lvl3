2b42323618bf84051007ca1dab27ca4a1757ee26
Vova <bkj@ya.ru>
2022-07-07 11:28:44 +0300

    add page_loader function

--- page_loader/main.py
+++ page_loader/main.py
@@ -1,2 +1,54 @@
+import argparse
+import requests
+import os
+from urllib.parse import urlparse
+
+
+def get_file_name(link):
+    """ Make a file name from a link"""
+    file_name = []
+    url = urlparse(link)
+    file_name.append(url.netloc.replace('.', '-'))
+    file_name.append(url.path.replace('/', '-'))
+    if not url.path.endswith('.html'):
+        file_name.append('.html')
+    return ''.join(file_name)
+
+
+def download(link, path):
+    """
+        Downdload a web-site page by a link and
+        save it to an existing directory.
+    """
+    if os.path.exists(path):
+        try:
+            r = requests.get(link)
+        except requests.RequestException as error:
+            print('Requests error:', error)
+        file_path = os.path.join(path, get_file_name(link))
+        with open(file_path, 'w') as f:
+            f.write(r.text)
+        return file_path
+    else:
+        raise NameError('A directory do no exist!')
+
+
 def page_loader():
-    print('Hello, World!')
+    """
+        Parse arguments for CLI: path, link.
+        Call download function with parsed arguments.
+        Print result - actual path of a dowloaded page.
+    """
+    parser = argparse.ArgumentParser(
+        description='Download a page by a web-site \
+         link and save it to a mentioned directory.'
+    )
+    parser.add_argument('link', metavar='LINK',
+                        type=str, help='a link on a page that should be loaded')
+    parser.add_argument('--output', metavar='PATH',
+                        dest='path', default=os.getcwd(), type=str,
+                        help='a directory path to save a page(should be exist) \
+                        (default: CWD)')
+    args = parser.parse_args()
+    file_path = download(args.link, args.path)
+    print(file_path)
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,24 +1,19 @@
-import subprocess
 import tempfile
 import os
+
+from page_loader import download
 
 
 link = 'https://ru.hexlet.io/courses'
 filename = 'ru-hexlet-io-courses.html'
 
 
-def get_fixtures_path(name):
-    return os.path.join('tests/fixtures', name)
-
-
 def test_page_loader(requests_mock):
     requests_mock.get(link, text='data')
     with tempfile.TemporaryDirectory() as tmpdir:
         expected_path = os.path.join(tmpdir, filename)
-        path = subprocess.run(
-            ['page-loader', '--output', tmpdir, link],
-            stdout=subprocess.PIPE, encoding='utf-8')
-        with open(expected_path) as f:
+        path = download(link, tmpdir)
+        assert expected_path == path
+        with open(path) as f:
             actual = f.read()
-        assert path == expected_path
         assert actual == 'data'
